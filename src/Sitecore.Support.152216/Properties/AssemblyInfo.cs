fe4650d9b38e0f54d24bdb6b241e6705d16b8084
Dmitry Kolinchuk <dmko@sitecore.net>
2017-03-16 11:40:22 +0200

    Prevents the Sitecore.CodeDom.Compiler.CompilerException exception that occurs when the WFFM module is installed and the Social Connected module is disabled

---
+++ src/Sitecore.Support.152216/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.152216")]
+[assembly: AssemblyProduct("Sitecore.Support.152216")]
+[assembly: ComVisible(false)]
